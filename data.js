0d318ad006848a4d5f92e189ad91a6d83d9bd000
Joe Dravarol <joedravarol@gmail.com>
2022-06-16 00:03:09 +0100

    feat: Implement routes for getting quotes

--- data.js
+++ data.js
@@ -1,3 +1,97 @@
 module.exports = {
-  
+  quotes: [
+    {
+      "film": "Princess Mononoke",
+      "quote": "You cannot alter your fate. However, you can rise to meet it.",
+    },
+    {
+      "film": "Kiki’s Delivery Service",
+      "quote": "We each need to find our own inspiration. Sometimes it’s not easy.",
+    },
+    {
+      "film": "From Up On Poppy Hill",
+      "quote": "There’s no future for people who worship the future, and forget the past. ",
+    },
+    {
+      "film": "Howl’s Moving Castle",
+      "quote": "They say that the best blaze burns brightest when circumstances are at their worst.",
+    },
+    {
+      "film": "The Cat Returns",
+      "quote": "Always believe in yourself. Do this and no matter where you are, you will have nothing to fear.",
+    },
+    {
+      "film": "The Wind Rises",
+      "quote": "Inspiration unlocks the future.",
+    },
+    {
+      "film": "The Secret World of Arrietty",
+      "quote": " It’s funny how you wake up each day and never really know if it’ll be one that will change your life forever.",
+    },
+    {
+      "film": "My Neighbor Totoro",
+      "quote": "Everybody, try laughing. Then whatever scares you will go away!",
+    },
+    {
+      "film": "Princess Mononoke",
+      "quote": "Life is suffering. It is hard. The world is cursed. But still you find reasons to keep on living.",
+    },
+    {
+      "film": "Castle in the Sky",
+      "quote": "No matter how many weapons you have, no matter how great your technology might be, the world cannot live without love! ",
+    },
+    {
+      "film": "Tales From Earthsea",
+      "quote": "Deny death, and you deny life.",
+    },
+    {
+      "film": "Kiki’s Delivery Service",
+      "quote": "Just follow your heart, and keep smiling.",
+    },
+    {
+      "film": "The Secret World of Arrietty",
+      "quote": "Sometimes you have to fight for the things that are worth fighting for.",
+    },
+    {
+      "film": "Howl’s Moving Castle",
+      "quote": "A heart’s a heavy burden.",
+    },
+    {
+      "film": "Castle in the Sky",
+      "quote": "The Earth speaks to all of us, and if we listen, we can understand.",
+    },
+    {
+      "film": "Spirited Away",
+      "quote": "Once you’ve met someone you never really forget them.",
+    },
+    {
+      "film": "My Neighbors the Yamadas",
+      "quote": "Life, as they say, has its ups and downs. At times, the waves may taunt you, tossing you in their swells. But take heart. It’s hard to stick with it and make it on your own. But even a couple of losers can survive most things if they’re together.",
+    },
+    {
+      "film": "Kiki’s Delivery Service",
+      "quote": "It doesn’t really matter what color your dress is. What matters is the heart inside.",
+    },
+    {
+      "film": "Howl’s Moving Castle",
+      "quote": "One thing you can always count on is that hearts change.",
+    },
+    {
+      "film": "The Cat Returns",
+      "quote": "Whenever someone creates something with all of their heart, then that creation is given a soul."
+    },
+    {
+      "film": "Whisper of the Heart",
+      "quote": "The rough stone is inside of you. You have to find it... and then polish it."
+    },
+    {
+      "film": "Tales from Earthsea",
+      "quote": "Deny death, and you deny life."
+    },
+    {
+      "film": "The Wind Rises",
+      "quote": "Who has seen the wind? Neither I nor you: But when the leaves hand trembling, The wind is passing through."
+    }
+  ]
 }
+
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
-const QUOTES_DATA = require('./data.js')
+const { quotes } = require('./data.js')
 const PORT = process.env.PORT || 8000;
 
 app.use(express.static('public'))
+
+app.get('/api/random', (req, res) => {
+  const randomIndex = Math.floor(Math.random() * quotes.length)
+
+  res.json(quotes[randomIndex])
+});
+
+app.get('/api/quotes', (req, res) => {
+  res.json(quotes)
+})
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`)
